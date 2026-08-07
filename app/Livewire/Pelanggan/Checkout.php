966e2f821008665d6faeec7a7d4977eeb6a1a76c
WafyPutraPangestu <wafy@raharja.info>
2026-08-07 11:06:41 +0700

    Fix htmlspecialchars array given in checkout by parsing Komerce structure

--- app/Livewire/Pelanggan/Checkout.php
+++ app/Livewire/Pelanggan/Checkout.php
@@ -245,9 +245,16 @@ class Checkout extends Component
                 $this->courier
             );
 
-            $this->courierServices = $costs;
+            $this->courierServices = collect($costs)->map(function ($service) {
+                return [
+                    'service'     => $service['service'],
+                    'description' => $service['description'] ?? '',
+                    'cost'        => $service['cost'][0]['value'] ?? 0,
+                    'etd'         => $service['cost'][0]['etd'] ?? '',
+                ];
+            })->toArray();
 
-            if (count($costs) === 0) {
+            if (count($this->courierServices) === 0) {
                 $this->notifyWarning('Layanan kurir tidak tersedia untuk rute tujuan ini.', 'LOGISTIK KOSONG');
             }
         } catch (\Exception $e) {
